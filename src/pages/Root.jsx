34d67b6a7709314466b9f15f3ac1860d171c5ae6
alielrayes <engali7assan@yahoo.com>
2023-01-05 09:40:53 +0200

    lesson5

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,20 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 
-
 const Root = () => {
   return (
     <div>
-      <Typography gutterBottom noWrap variant="h4" component="p">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem ipsum sunt voluptate possimus impedit fuga aliquid omnis explicabo, voluptatem quasi quisquam nemo repellat illo. Eum nisi modi ab labore voluptatibus.
+      <Typography color="secondary.main" variant="h4">
+        Ali hassan
       </Typography>
-
-
-
- <Typography variant="h4"  > aaaa </Typography>
-
-
-
 
       <Outlet />
     </div>
