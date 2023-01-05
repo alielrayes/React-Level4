120719f5a468fca3df5814d03018ed2a567b439a
alielrayes <engali7assan@yahoo.com>
2023-01-05 14:06:36 +0200

    lesson8

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,32 +1,29 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import { Delete } from "@mui/icons-material";
-import { IconButton, Link } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
+import { Toolbar, AppBar, Avatar, Link, Typography } from "@mui/material";
 
 const Root = () => {
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
+          <Link
+            sx={{
+              flexGrow: 1,
+              textDecoration: "none",
+              "&:hover": { fontSize: "16.5px" },
+            }}
+            color="inherit"
+            href="/"
+          >
+            My expenses
+          </Link>
 
+          <Typography mr={2} variant="body1" color="inherit">
+            Ali Hassan
+          </Typography>
 
-     
-
-
-          <Link   sx={{ flexGrow: 1, textDecoration: "none" }} color="inherit" href="/">  My expenses  </Link>
-
-
-         
-
-          <Button color="inherit">Login</Button>
-
-
+          <Avatar alt="Remy Sharp" src="./imges/Ali Hassan.JPG" />
         </Toolbar>
       </AppBar>
 
