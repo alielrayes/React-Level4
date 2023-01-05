b90a554ed48b4862b04573278fb230a4e2f7a8a9
alielrayes <engali7assan@yahoo.com>
2023-01-05 11:05:53 +0200

    lesson6

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Delete } from "@mui/icons-material";
+import { IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Root = () => {
   return (
@@ -8,6 +12,20 @@ const Root = () => {
       <Typography color="secondary.main" variant="h4">
         Ali hassan
       </Typography>
+
+      <Button
+        sx={{ mr: "auto", ml: "auto", display: "flex" }}
+        endIcon={<Delete />}
+        variant="contained"
+        color="error"
+      >
+        Click here
+      </Button>
+
+
+      <IconButton size="large" color="info" >
+        <MenuIcon fontSize="inherit" />
+      </IconButton>
 
       <Outlet />
     </div>
